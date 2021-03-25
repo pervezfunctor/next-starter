18ad66e164818d70b0d51b952c74137f85e69d8c
Pervez Iqbal <pervezfunctor@gmail.com>
2021-03-25 19:57:00 +0530

    update to the latest next

--- pages/Index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import '../sass/styles.scss'
-
-const App = () => <h1 className="is-text is-size-1">Hello, World</h1>
-
-export default App
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function App() {
+  return <h1 className="is-text is-size-1">Hello, World</h1>
+}
+
+export default App
