d389f61e5b9023a6cc837fca9cb18316fa99478f
Pervez Iqbal <pervezfunctor@gmail.com>
2021-12-15 19:51:27 +0530

    msw config

---
+++ mocks/index.ts
@@ -0,0 +1,17 @@
+if (typeof window === 'undefined') {
+  const { server } = require('./server')
+  server.listen({
+    onUnhandledRequest: ({ method, url }) => {
+      if (
+        url.pathname.startsWith('/api') ||
+        url.pathname.startsWith('/_next')
+      ) {
+        return
+      }
+      throw new Error(`Unhandled ${method} request to ${url}`)
+    },
+  })
+} else {
+  const { worker } = require('./browser')
+  worker.start()
+}
